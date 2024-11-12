5c36f3f7b7bb5a5ec0f84125a1d39822cbd3f203
jmorga302 <jmorga302@caledonian.ac.uk>
2024-11-12 01:49:30 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,24 +1,66 @@
-import java.util.Scanner;
-
-class Dec2Hex
+public class Dec2Hex
 {
-public static int Arg1;
-    public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    public static String convertToHex(String Numx) {
+
+
         int rem, num;
-        num = Arg1;
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        boolean negative = false;
+        try{
+            if(Numx.trim().isEmpty()){ //checks if the string doesnt contain any information
+                System.out.println("Blank values can't be converted.");
+                throw new Exception();
+            }
+            num = Integer.parseInt(Numx); //converts the passed argument into an integer to be converted to a hexadecimal number
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+            if( num == 0) //sets the hexadecimal value to zero as the coversion to hexadecimal doesn't take 0 into account
+            {
+                hexadecimal = "0";
+            }
+            else{
+                if(num <0){ // checks if the value is negative and converts it to positive as the hex conversion doesnt work with negative values
+                    negative = true;
+                    num = num * -1;
+                }
+                while(num != 0)//converts integer to hexadecimal
+                {
+                    rem=num%16;
+                    hexadecimal= ch[rem] + hexadecimal;
+                    num = num/16;
+
+                }
+            }
+            if(negative){
+                hexadecimal = "-" + hexadecimal;
+            }
+
+
+            System.out.println("Hexadecimal representation is: " + hexadecimal + ".");
+            return hexadecimal;
         }
+        catch(Exception e) //if the value entered is an invalid data type such as a string or a decimal number then this code will execute
+        {
+            System.out.println(Numx +"is not a valid integer.");
+            return "invalid input";
+        }
+    }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+    public static void main(String args[])    {
+
+
+        if (args.length == 0) { //this checks that the user has entered anything into the command line
+            System.out.println("Please provide an input as a number as a command-line argument.");
+            return;
+        }
+        else{
+            for(String i: args){
+                convertToHex(i); //values are passed in as a string as thats how arguments are accepted from the command line
+            }
+        }
 
     }
 }
+
